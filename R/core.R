457deed0984c664b0fb3e08fbcda69a756224c76
Siqi Zhang <iqis.gnahz@gmail.com>
2018-12-08 23:26:30 -0500

    clean-up & reorganize.

--- R/core.R
+++ R/core.R
@@ -117,3 +117,34 @@ collect.soda <- function(x, ...){
   res
 }
 
+
+# TODO add `very_rich` option, through which queries metadata on assets through "identifier" field
+# and merge with original data.
+
+#' Metadata on datasets on a Socrata domain
+#'
+#' @import httr jsonlite
+#' @export
+#' @param domain a Socrata domain
+#' @param rich get rich metadata
+#' @return  a data frame
+catalog <- function(domain, rich = FALSE){
+  # if (very_rich == TRUE) rich <- TRUE
+  request <- httr::parse_url(add_protocol(domain))
+  request$path <- "data.json"
+
+  res <- jsonlite::fromJSON(response_content.soda(request))$dataset
+
+  if (!rich) {
+    res <- dplyr::select(res, "title", "description", "issued", "modified", "theme", "identifier")
+  }
+
+  res$domain <- domain
+  # substr_uuid <- function(x) substr(x, (nchar(x) - 8), nchar(x))
+  # res$uuid <- substr_uuid(res$identifier)
+  res$uuid <- gsub("^.*/", "", res$identifier)
+
+  res$theme <- as.character(res$theme)
+
+  res
+}
--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -8,28 +8,3 @@ test_that("authentication works", {
 })
 
 
-
-# earthquake <-
-#   soda("https://soda.demo.socrata.com/resource/4tka-6guv") %>%
-#   credential(
-#     username = "mark.silverberg+soda.demo@socrata.com",
-#     password = "7vFDsGFDUG",
-#     app_token = "JnDigAX3zQtqQIgBJvVlMLgm8")
-#
-# earthquake_query1 <- earthquake %>%
-#   query("SELECT *
-#              WHERE depth > 15 AND source = 'US'
-#              ORDER BY depth") %>%
-#   collect()
-#
-#
-# earthquake_query2 <- earthquake %>%
-#   filter(depth > 20, source == 'US') %>%
-#   collect()
-#
-#
-# earthquake_query3 <- earthquake %>%
-#   group_by(source) %>%
-#   summarize(max_depth = max(depth)) %>%
-#   filter(depth > 20) %>%
-#   collect()
